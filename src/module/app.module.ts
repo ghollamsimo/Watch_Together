feb4a8055775235770b076aa7e291138ff62f4fc
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-20 11:37:11 +0100

    Feat --> Adding JwtModule for return the token

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -4,11 +4,22 @@ import { User, UserModelSchema } from '../infrastructure/db/schemas/user.schema'
 import { UserController } from '../interface/http/user.controller';
 import { UserUseCase } from '../application/usecases/user.usecase';
 import { UserRepositoryImpl } from '../infrastructure/repositories/user.repository.impl';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/Watchtogether'),
+    ConfigModule.forRoot(),
     MongooseModule.forFeature([{ name: User.name, schema: UserModelSchema }]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      })
+    }),
   ],
   controllers: [UserController],
   providers: [
