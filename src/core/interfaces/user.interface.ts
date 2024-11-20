a453e6c4f08141eeed52efe406b6e52c4bc28e14
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-20 11:32:28 +0100

    Feat --> Remove forgotPassword and resetPassword method

--- src/core/interfaces/user.interface.ts
+++ src/core/interfaces/user.interface.ts
@@ -5,6 +5,4 @@ export interface UserInterface {
     store(user: UserEntity): Promise<UserEntity>
     index(user: UserEntity): UserEntity
     login(loginDTO: LoginDTO): Promise<{ token: string }>;
-    forgotPassword(email: string): Promise<{ email: string }>;
-    resetPassword(resetToken: string, newPassword: string): Promise<{ message: string }>;
 }
