84adb17a9a342e22504c22c8dc1835f95c81590c
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-22 14:23:06 +0100

    Feat --> Adding logic of method delete and index

--- src/infrastructure/repositories/playlist.repository.impl.ts
+++ src/infrastructure/repositories/playlist.repository.impl.ts
@@ -1,11 +1,30 @@
 import { InjectModel } from "@nestjs/mongoose";
 import { PlaylistEntity } from "src/core/entities/playlist.entity";
 import { PlaylistInterface } from "src/core/interfaces/playlist.interface";
-import { Playlist as  PlaylistDocument} from "../db/schemas/playlist.schema";
+import { PlaylistDocument, Playlist} from "../db/schemas/playlist.schema";
 import { Model } from "mongoose";
+import { NotFoundException } from "@nestjs/common";
 
 export class PlaylistRepositoryImpl implements PlaylistInterface{
-    constructor(@InjectModel(PlaylistDocument.name) public readonly playListModel: Model<PlaylistDocument>){}
+    constructor(@InjectModel(Playlist.name) public readonly playListModel: Model<PlaylistDocument>){}
+    async delete(id: string, user_id: string): Promise<{ message: string }> {
+        const playlist = await this.playListModel.findById(id).exec();
+        if (!playlist) {
+          throw new NotFoundException('Playlist not found');
+        }
+    
+        const playlistDeleteResult = await this.playListModel.findOneAndDelete({
+          _id: id,
+          user_id: user_id,
+        }).exec();
+    
+        if (!playlistDeleteResult) {
+          throw new NotFoundException('No Playlist found with the specified id and user_id');
+        }
+    
+        return { message: 'Playlist deleted' };
+      }
+    
     async store(playlistEntity: PlaylistEntity): Promise<PlaylistEntity> {
         const playlist = new this.playListModel({
             user_id: playlistEntity.user_id,
@@ -20,8 +39,8 @@ export class PlaylistRepositoryImpl implements PlaylistInterface{
             savedPlaylist.videos
         )
     }
-    index(playlistEntity: PlaylistEntity): Promise<PlaylistEntity> {
-        throw new Error("Method not implemented.");
+    index(): Promise<PlaylistDocument[]> {
+        return this.playListModel.find().exec()
     }
     
 }
